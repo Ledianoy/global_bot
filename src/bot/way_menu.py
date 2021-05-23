7b473cca4abc8d22b3c2d6ee164912ccadb60c19
sergey <Ledovskiys@gmailcom>
2021-05-23 11:28:44 +0300

    alfa - added channel viewing function

---
+++ src/bot/way_menu.py
@@ -0,0 +1,20 @@
+from bot.db.work_user import set_user_auth_state
+from bot.send import send_a_request_user
+from bot.telegram.types import Update
+
+
+async def main_menu(update: Update):
+    user_id = update.message.from_.id
+    reply_to_message_id = (
+        "Тебя узнали!\n"
+        "Команды настроек: \n"
+        " 1 : Работа с каналами \n"
+        " 2 : Добавление запрещенного слова"
+    )
+
+    await set_user_auth_state(user_id, 2)
+    await send_a_request_user(
+        chat_id=update.message.chat.id,
+        text=reply_to_message_id,
+    )
+    return
